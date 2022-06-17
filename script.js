1cdaa140571ad32ec731cac332cac82cea255d5d
Regina399 <76470773+Regina399@users.noreply.github.com>
2022-06-17 10:44:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').
+    value.length == 0){
+        alert("Пожалуйста введите задачу!")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML
+        +=`
+        <div class ="task">
+        <span id="taskname">
+            ${document.querySelector('#newtask input').value}
+        </span>
+        <button class="delete">
+            <img src="1497559425-delete_84991.svg">
+        </button>
+        </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i = 0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+            this.parentNode.remove();
+        }
+            }
+
+        var tasks = document.querySelectorAll(".tasks");
+        for( var i=0; i<tasks.lenght; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('.completed');
+            }
+        }
+
+        document.querySelector("#newtasks input").
+        value = ""
+}
+}
